932fbc21e746ca5f937da4509a06c950bd068e29
Alina Mathew <amathew6@luc.edu>
2026-02-11 15:47:56 -0600

    made it longer but more readable

--- DNA.py
+++ DNA.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 s = open('rosalind_dna.txt','r').read()
-for n in ["A","C","G","T"]:
-    print(s.count(n), end=' ')
+a = s.count("A")
+c = s.count("C")
+g = s.count("G")
+t = s.count("T")
+print(a, c, g, t)
